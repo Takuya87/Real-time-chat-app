db35fc8fca32ece7790e26aa6232b698f7414b30
Takuya87 <87933851+Takuya87@users.noreply.github.com>
2021-10-11 23:56:57 +0900

    Update firebase.js

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -4,13 +4,13 @@ import 'firebase/firestore'
 
 if (!firebase.apps.length) {
     const config = {
-        apiKey: "AIzaSyBuXiUOK0tHZvnSKHxm0XIdq8TDtkRFCvg",
-        authDomain: "slack-demo-app-f3474.firebaseapp.com",
-        projectId: "slack-demo-app-f3474",
-        storageBucket: "slack-demo-app-f3474.appspot.com",
-        messagingSenderId: "689508929770",
-        appId: "1:689508929770:web:ac6e246887a6d9e5418793",
-        measurementId: "G-48GSWHFEBB"
+        apiKey: "××××××××××××××××××××××××××",
+        authDomain: "××××××××××××××××××××××××××",
+        projectId: "××××××××××××××××××××××××××",
+        storageBucket: "××××××××××××××××××××××××××",
+        messagingSenderId: "××××××××××××××××××××××××××",
+        appId: "××××××××××××××××××××××××××",
+        measurementId: "××××××××××××××××××××××××××"
     }
     firebase.initializeApp(config)
 }
